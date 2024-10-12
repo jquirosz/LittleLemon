23ea479dd3300664a22768442c1891942dd41c8a
jquirosz <pabloQZ12@gmail.com>
2024-10-12 14:37:05 -0600

    Adding initial endpoints

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+
+    path('menu/', views.MenuViewSet.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+
+    path('booking/', views.BookingViewSet.as_view()),
+    path('booking/<int:pk>', views.SingleBookingItemView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from rest_framework import viewsets, generics
+from rest_framework.decorators import api_view
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class BookingViewSet(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class SingleBookingItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class MenuViewSet(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
